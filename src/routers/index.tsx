9af83fa0c85e96c76eef2c346e2c8ec53cc1b801
Felipe Brenner <felipe.brenner@ab-inbev.com>
2024-10-27 19:02:57 -0300

    feat: improve routers component

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -4,43 +4,23 @@ import { Calendar, Home, Login, Profile } from "@pages";
 import type { Router } from "@remix-run/router";
 import { createBrowserRouter } from "react-router-dom";
 
+const createRoute = (path: string, element: JSX.Element) => ({
+  path,
+  element: <AuthGuard isLogin>{element}</AuthGuard>,
+});
+
+const createLayoutRoute = (path: string, element: JSX.Element) => ({
+  path,
+  element: (
+    <AuthGuard>
+      <Layout>{element}</Layout>
+    </AuthGuard>
+  ),
+});
+
 export const routers: Router = createBrowserRouter([
-	{
-		path: pathRoutes.login,
-		element: (
-			<AuthGuard isLogin>
-				<Login />
-			</AuthGuard>
-		),
-	},
-	{
-		path: pathRoutes.home,
-		element: (
-			<AuthGuard>
-				<Layout>
-					<Home />
-				</Layout>
-			</AuthGuard>
-		),
-	},
-	{
-		path: pathRoutes.calendar,
-		element: (
-			<AuthGuard>
-				<Layout>
-					<Calendar />
-				</Layout>
-			</AuthGuard>
-		),
-	},
-	{
-		path: pathRoutes.perfil,
-		element: (
-			<AuthGuard>
-				<Layout>
-					<Profile />
-				</Layout>
-			</AuthGuard>
-		),
-	},
+  createRoute(pathRoutes.login, <Login />),
+  createLayoutRoute(pathRoutes.home, <Home />),
+  createLayoutRoute(pathRoutes.calendar, <Calendar />),
+  createLayoutRoute(pathRoutes.perfil, <Profile />),
 ]);
